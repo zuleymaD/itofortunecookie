694033b79eab6b3a47394c57a1dc40795961ccf5
zuleyma <zuleymade.vazquez@gmail.com>
2016-10-19 17:23:49 -0500

    server de texto con nodemon

---
+++ server.js
@@ -0,0 +1,35 @@
+//cargando modulo http
+var http = require('http');
+//cargando la libreria colores
+var colors = require('colors');
+//generar un tema
+colors.setTheme({
+silly : 'rainbow',
+input : 'grey',
+verbose : 'cyan',
+prompt : 'grey',
+info : 'green',
+data : 'grey',
+help : 'cyan',
+warn : ['yellow','rainbow'],
+debug : 'blue',
+error : 'red',
+achivement : 'rainbow'
+});
+//obteniendo configuraciones
+var PORT = 3000;
+var IP ='127.0.0.1';    
+//creando el server
+var server = http.createServer(function(req, res){
+    res.writeHead(200,{
+        'content-type' : 'text/plain',
+        'server' : 'Buho@0.0.0'
+    });
+    res.write('hola....');
+    res.end();
+
+});
+//poniendo a escuchar el server
+server.listen(PORT, IP, function(){
+    console.log(`>server escuchado en http://${IP}:${PORT}/`.info);
+});
